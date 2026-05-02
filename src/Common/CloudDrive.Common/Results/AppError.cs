ff68c4bf2f8718adf3bc299a8cf1c56cc60b6987
Александр Антонов <antonovsasha637@gmail.com>
2026-05-02 08:20:05 +0300

    feat(Common): AppError & Result - Add AppError - Add Result

---
+++ src/Common/CloudDrive.Common/Results/AppError.cs
@@ -0,0 +1,36 @@
+﻿namespace CloudDrive.Common.Results;
+
+public sealed record AppError
+{
+	private AppError(
+		string code,
+		string description)
+	{
+		Code = code;
+		Description = description;
+	}
+
+	public string Code { get; }
+	public string Description { get; }
+
+
+	public static readonly AppError None = new(string.Empty, string.Empty);
+
+	public static AppError Failure(string code, string description) =>
+		new(code, description);
+
+	public static AppError NotFound(string code, string description) =>
+		new(code, description);
+
+	public static AppError Validation(string code, string description) =>
+		new(code, description);
+
+	public static AppError Conflict(string code, string description) =>
+		new(code, description);
+
+	public static AppError UnAuthorized(string code, string description) =>
+		new(code, description);
+
+	public static AppError Forbidden(string code, string description) =>
+		new(code, description);
+}
---
+++ src/Common/CloudDrive.Common/Results/Result.cs
@@ -0,0 +1,52 @@
+﻿namespace CloudDrive.Common.Results;
+
+public class Result
+{
+	public bool IsSuccess { get; }
+	public bool IsFailure => !IsSuccess;
+	public AppError Error { get; }
+
+	protected Result(bool isSuccess, AppError error)
+	{
+		if (isSuccess && error != AppError.None)
+			throw new ArgumentException("Success result cannot have an error", nameof(error));
+		if (!isSuccess && error == AppError.None)
+			throw new ArgumentException("Failure result must have an error", nameof(error));
+
+
+		IsSuccess = isSuccess;
+		Error = error;
+	}
+
+	public static Result Success() => new(true, AppError.None);
+	public static Result Failure(AppError error) => new(false, error);
+
+
+	public static implicit operator Result(AppError error) => Failure(error);
+}
+
+public class Result<TValue> : Result
+{
+	private readonly TValue? _value;
+	public TValue Value => IsSuccess
+		? _value!
+		: throw new InvalidOperationException("Cannot access Value of a failed result");
+
+	internal Result(TValue value)
+		: base(true, AppError.None)
+	{
+		_value = value;
+	}
+
+	internal Result(AppError error)
+		: base(false, error)
+	{
+		_value = default;
+	}
+
+	public static Result<TValue> Success(TValue value) => new(value);
+	public new static Result<TValue> Failure(AppError error) => new(error);
+
+	public static implicit operator Result<TValue>(TValue value) => Success(value);
+	public static implicit operator Result<TValue>(AppError error) => Failure(error);
+}
