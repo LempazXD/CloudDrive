0c52b1fd67cf52085f9742f68a10d926a873dadf
Александр Антонов <antonovsasha637@gmail.com>
2026-06-01 17:06:08 +0300

    feat(project): use injectable TimeProvider - Register TimeProvider.System as a singleton in the DI container

--- src/Bootstrapper/CloudDrive.Api/Program.cs
+++ src/Bootstrapper/CloudDrive.Api/Program.cs
@@ -3,6 +3,7 @@ using CloudDrive.Shared.Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+	.AddSingleton(TimeProvider.System)
 	.AddProblemDetailsConfiguration()
 	.AddGlobalExceptionHandling()
 	.AddRequestLocalizationConfiguration();
